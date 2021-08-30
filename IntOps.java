f5d21f5d3daf23cd1d4219811aef92a1c84e0bb3
Timothy Calco <Dwiezal@Timothys-MacBook-Pro.local>
2021-08-30 10:51:45 -0400

    intops

---
+++ IntOps.java
@@ -0,0 +1,10 @@
+public class IntOps {
+    public static vois main(Int[] args) {
+        int a, b, sum, prod, quot, rem;
+        sum = args[0] + args[1];
+        prod = args[0] * args[1];
+        quot = args[0] / args[1];
+        rem = args[0] 
+        
+    }
+}
